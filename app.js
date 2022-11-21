bcb839e0618118dc69fb3f03affde11a5cf3c59c
Milena Sowinska <mil.sowinska@gmail.com>
2022-11-21 21:00:52 +0100

    created new library

--- app.js
+++ app.js
@@ -1,63 +1,13 @@
 "use strict";
-class Book {
-    constructor(title, autor, yearRealease, pages, booked) {
-        this.title = title;
-        this.autor = autor;
-        this.yearRealease = yearRealease;
-        this.pages = pages;
-        this.booked = booked;
-    }
-    czytajKsiazke() {
-        console.log('Title : ' + this.title);
-    }
-}
-class Library {
-    constructor(name, adress) {
-        this.listOfBooks = [];
-        this.name = name;
-        this.adress = adress;
-    }
-    addBook(book) {
-        this.listOfBooks.push(book);
-    }
-    deleteBook(titleBook) {
-        // let newList: Book[] = [];
-        this.listOfBooks = this.listOfBooks.filter((value, index, array) => {
-            return value.title !== titleBook;
-        });
-        // this.listOfBooks = newList;
-    }
-    findBook(titleBook) {
-        let result = this.listOfBooks.find((book, index, array) => {
-            return book.title === titleBook;
-        });
-        return result;
-    }
-}
-let library = new Library("Ump", "Poznań");
-let library2 = new Library("Uam", "Warszawa");
 function addBookToLibrary() {
-    let harryPotter = new Book("Harry potter", "J.K Rowling", 2000, 950, false);
-    harryPotter.czytajKsiazke();
-    let wladcaPierscieni = new Book("Władca pierścienia", "xyz", 1997, 652, false);
-    wladcaPierscieni.czytajKsiazke();
-    library.addBook(harryPotter);
-    library.addBook(wladcaPierscieni);
+    console.log("add to library");
 }
 function deleteBookFromLibrary() {
-    library.deleteBook('Harry potter');
+    console.log("delete book");
 }
 function showListOfLibrary() {
-    console.log(library);
-    console.log(library2);
+    console.log("show list of books");
 }
 function moveBooksBetweenLibrary() {
-    let znalezione = library.findBook("Harry potter");
-    if (znalezione !== undefined) {
-        library.deleteBook(znalezione === null || znalezione === void 0 ? void 0 : znalezione.title);
-        library2.addBook(znalezione);
-    }
-    else {
-        alert("nie ma książki");
-    }
+    console.log("move book");
 }
--- app.ts
+++ app.ts
@@ -1,86 +1,12 @@
-
-class Book {
-    title: string;
-    autor: string;
-    yearRealease: number;
-    pages: number;
-    booked: boolean;
-
-    constructor(title: string, autor: string, yearRealease: number, pages: number, booked: boolean) {
-        this.title = title;
-        this.autor = autor;
-        this.yearRealease = yearRealease;
-        this.pages = pages;
-        this.booked = booked;
-    }
-   czytajKsiazke(){
-       console.log('Title : ' + this.title)
-   }
+function addBookToLibrary (){
+    console.log("add to library")
 }
-class Library {
-    name: string;
-    adress: string;
-    listOfBooks: Book[] = [];
-
-    constructor(name: string, adress: string) {
-        this.name = name;
-        this.adress = adress;
-    }
-
-    addBook(book: Book): void {
-        this.listOfBooks.push(book)
-    }
-
-    deleteBook(titleBook: string): void {
-        // let newList: Book[] = [];
-        this.listOfBooks = this.listOfBooks.filter((value: Book, index: number, array: Book[]) => {
-            return value.title !== titleBook
-        })
-        // this.listOfBooks = newList;
-
-    }
-    findBook(titleBook: string): Book | undefined {
-        let result = this.listOfBooks.find((book: Book, index: number, array: Book[]) => {
-            return book.title === titleBook
-        })
-        return result
-    }
-
+function deleteBookFromLibrary(){
+    console.log("delete book")
 }
-
-let library: Library = new Library("Ump", "Poznań")
-let library2: Library = new Library("Uam", "Warszawa")
-
-
-
-
-function addBookToLibrary(): void {
-    let harryPotter: Book = new Book("Harry potter", "J.K Rowling", 2000, 950, false)
-    harryPotter.czytajKsiazke();
-
-    let wladcaPierscieni: Book = new Book("Władca pierścienia", "xyz", 1997, 652, false)
-    wladcaPierscieni.czytajKsiazke()
-
-    library.addBook(harryPotter);
-    library.addBook(wladcaPierscieni);
+function showListOfLibrary() {
+    console.log("show list of books")
 }
-
-function deleteBookFromLibrary(): void {
-    library.deleteBook('Harry potter')
-
+function moveBooksBetweenLibrary(){
+    console.log("move book")
 }
-function showListOfLibrary(): void {
-    console.log(library);
-    console.log(library2)
-}
-function moveBooksBetweenLibrary(): void {
-    let znalezione: Book | undefined = library.findBook("Harry potter");
-    if (znalezione !== undefined) {
-        library.deleteBook(znalezione?.title);
-        library2.addBook(znalezione)
-    } else {
-        alert("nie ma książki")
-    }
-}
-
-
