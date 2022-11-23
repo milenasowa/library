d57269b8cc27962da766fa80cd4e26a4297debd2
Milena Sowinska <mil.sowinska@gmail.com>
2022-11-23 22:53:16 +0100

    debbuging

--- app.js
+++ app.js
@@ -1,62 +1,85 @@
 "use strict";
+// ??
+// ??
 function addBookToLibrary() {
+    // ??
     console.log("add to library");
 }
+// ??
 function deleteBookFromLibrary() {
+    // ??
     console.log("delete book");
 }
+// ??
 function showListOfLibrary() {
+    // ??
     console.log("show list of books");
 }
+// ??
 function moveBooksBetweenLibrary() {
+    // ??
     console.log("move book");
 }
+// ??
 class Book {
+    // ??
     constructor(title, author, numberOfPages) {
+        // ??
         this.title = title;
+        // ??
         this.author = author;
+        // ??
         this.numberOfPages = numberOfPages;
     }
 }
+// ??
 let aniaZZielonegoWzgorza = new Book("Ania z Zielonego Wzgórza", "L.Mound-Montgomery", 700);
+// ??
 let perswazje = new Book("Perswazje", "Jane Austen", 550);
+// ??
 let shrek = new Book("Shrek", "J.Talkien", 300);
+// ??
 let potop = new Book("Potop", "Sienkiewicz", 1000);
+// ??
 let dziady = new Book("Dziady", "Mickiewicz", 500);
+// ??
 let wesele = new Book("Wesele", "Ktoś", 200);
+// ??
 class Library {
-    constructor(name, city, numberOfShelfs) {
+    // ??
+    constructor(name, city, numberOfShelfs, listofBooks) {
+        this.listofBooks = [];
+        let igor = "jest mistrzem";
+        // ??
         this.name = name;
-        this.city = city;
+        // ??
+        this.city = igor;
+        // ??
         this.numberOfShelfs = numberOfShelfs;
+        // ??
+        this.listofBooks = listofBooks;
+        // ??     
     }
 }
-// let listOfComedyBooks  = [
-//     potop,
-//     dziady,
-// ]
-// let listOfFantasyBooks = [
-// aniaZZielonegoWzgorza,
-// perswazje,
-// shrek,
-// ]
-let bibliotekaPoznanFantasy = new Library("Biblioteka fantasy", "Poznań", 3);
-let bibliotekaMiliczDrama = new Library("Biblioteka drama", "Milicz", 5);
+// ?? 
+let list1 = [aniaZZielonegoWzgorza, perswazje];
+// ??
+let bibliotekaPoznanFantasy = new Library("Biblioteka fantasy", "Poznań", 3, list1);
+// ??
+let bibliotekaMiliczDrama = new Library("Biblioteka drama", "Milicz", 5, [potop]);
+// ??
 function showBookInformation() {
+    // ??
     console.log(aniaZZielonegoWzgorza);
 }
+// ??
 function showFantasyLibrary() {
+    // ??
     console.log(bibliotekaPoznanFantasy);
 }
+// ??
 function showSecondLibrary() {
-    console.log(bibliotekaPoznanFantasy, bibliotekaMiliczDrama);
+    //
+    console.log(bibliotekaMiliczDrama);
 }
-class toy {
-    constructor(name) {
-        this.listOfToys = [];
-        this.name = name;
-        this.listOfToys.push("misiowate");
-    }
-}
-let mis = new toy("Miś");
-console.log(mis);
+//u-61
--- app.ts
+++ app.ts
@@ -1,20 +1,33 @@
-
+// ??
+// ??
 function addBookToLibrary (){
+// ??
     console.log("add to library")
 }
+// ??
 function deleteBookFromLibrary(){
+    // ??
     console.log("delete book")
 }
+// ??
 function showListOfLibrary() {
+    // ??
     console.log("show list of books")
 }
+// ??
 function moveBooksBetweenLibrary(){
+    // ??
     console.log("move book")
 }
+// ??
 class Book {
+    // ??
     constructor(title: string, author: string, numberOfPages: number){
+     // ??
      this.title = title;
+     // ??
      this.author= author;
+     // ??
      this.numberOfPages=numberOfPages;
      
     }
@@ -24,79 +37,62 @@ class Book {
     
     
 }
-let aniaZZielonegoWzgorza = new Book ("Ania z Zielonego Wzgórza","L.Mound-Montgomery", 700);
+// ??
+let aniaZZielonegoWzgorza: Book = new Book ("Ania z Zielonego Wzgórza","L.Mound-Montgomery", 700);
+// ??
 let perswazje = new Book ("Perswazje", "Jane Austen", 550);
+// ??
 let shrek = new Book ( "Shrek", "J.Talkien", 300);
+// ??
 let potop = new Book ("Potop", "Sienkiewicz", 1000);
+// ??
 let dziady = new Book ("Dziady", "Mickiewicz", 500);
+// ??
 let wesele = new Book ("Wesele", "Ktoś", 200);
-
+// ??
 class Library {
-
-    constructor(name:string, city:string, numberOfShelfs: number){
+    // ??
+    constructor(name:string, city:string, numberOfShelfs: number, listofBooks: Book [] ){
+        let igor: string = "jest mistrzem"
+        // ??
         this.name = name;
-        this.city = city;
+        // ??
+        this.city = igor;
+        // ??
         this.numberOfShelfs = numberOfShelfs;
-       
+        // ??
+        this.listofBooks = listofBooks;
+        // ??     
 
     }
     name: string;
     city: string;
     numberOfShelfs : number;
- 
-    
-
-    
-    
-//     listOfFantasyBooks : any = 
-//         aniaZZielonegoWzgorza,
-//         perswazje,
-//         shrek,
-//    ]
-//    listOfComedyBooks: any = [
-//     potop,
-//     dziady,
-//  ]
-   
+    listofBooks: Book [] = [];
+    // let milenka:string = "jest osłem"
     
 }
 
-// let listOfComedyBooks  = [
-//     potop,
-//     dziady,
-// ]
-// let listOfFantasyBooks = [
-// aniaZZielonegoWzgorza,
-// perswazje,
-// shrek,
-// ]
-let bibliotekaPoznanFantasy = new Library ("Biblioteka fantasy", "Poznań", 3, )
-;
-let bibliotekaMiliczDrama = new Library ("Biblioteka drama", "Milicz", 5,);
+// ?? 
+let list1 : Book[] = [aniaZZielonegoWzgorza, perswazje]
+// ??
+let bibliotekaPoznanFantasy = new Library ("Biblioteka fantasy", "Poznań", 3,  list1);
+// ??
+let bibliotekaMiliczDrama = new Library ("Biblioteka drama", "Milicz", 5,[potop]);
+// ??
 function showBookInformation() {
+    // ??
     console.log(aniaZZielonegoWzgorza)
 }
-
+// ??
 function showFantasyLibrary(){
+    // ??
     console.log(bibliotekaPoznanFantasy)
 }
+// ??
 function showSecondLibrary(){
-    console.log(bibliotekaPoznanFantasy, bibliotekaMiliczDrama)
+    //
+    console.log(bibliotekaMiliczDrama)
 }
 
-
-class toy {
-    constructor(name:string){
-        this.name = name
-        this.listOfToys.push("misiowate")
-    }
-    name: string
-    listOfToys : any =[]
-    
-
-
-}
-
-let mis = new toy ("Miś")
-console.log(mis)
-   
+//u-61
