8382d71918ec37987bd994de5122f3af12801cf7
Milena Sowinska <mil.sowinska@gmail.com>
2022-11-27 20:51:22 +0100

    added object by functions

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ class Product {
         this.value = value;
     }
 }
+let listaMagazynowa = [];
 let bread = new Product("chleb", 200, "g", true);
 let cola = new Product("cola", 2, "l", true);
 let butter = new Product("masło", 250, "g", true);
 let milk = new Product("mleko", 1, "l", false);
 let chocolade = new Product("czekolada", 100, "g", true);
 let water = new Product("woda", 1.5, "l", true);
+listaMagazynowa.push(bread);
+listaMagazynowa.push(cola);
+listaMagazynowa.push(butter);
+listaMagazynowa.push(milk);
+listaMagazynowa.push(chocolade);
+listaMagazynowa.push(water);
 class Shop {
     constructor(name, city, listOfProducts) {
         this.listOfProducts = [];
@@ -20,15 +27,48 @@ class Shop {
         this.city = city;
         this.listOfProducts = listOfProducts;
     }
+    getProducts() {
+        // sprawdzic czy ten uzytkownik ma uprawnienia do pobrania produktow
+        return this.listOfProducts;
+    }
+    existProduct(productName) {
+    }
+    setProducts(newListOfProducts) {
+        this.listOfProducts = newListOfProducts;
+    }
+    /**
+     *
+     *
+     */
+    addProductToStore(product) {
+        this.listOfProducts.push(product);
+    }
 }
 let biedronkaListOfProducts = [bread, cola, butter];
 let miliczListOfProduct = [milk, chocolade, water];
 let biedronka = new Shop("Biedronka", "Szamotuły", biedronkaListOfProducts);
 let auchan = new Shop("Auchan", "Milicz", miliczListOfProduct);
-function addProductToShop(item) {
-    // let newProduct: Product = new Product("piwo", 500, "ml", true)
-    biedronka.listOfProducts.push(item);
-    console.log(biedronka);
+/**
+ * 1.utworzyc liste magazynowa
+ * 2. wyszukać na liscie magazynowej produkt mający name === parametr funcji
+ * 3. sprawdzić czy szukany produkt istnieje
+ * a) istnieje, to dodać produkt do biedronki produkt za pomoca funkcji add product to store
+ * b) nie istnieje - alert "nie ma takiego produktu"
+ *
+ */
+function addProductToShop(product) {
+    let szukanyProdukt;
+    listaMagazynowa.forEach((item) => {
+        if (item.name === product) {
+            szukanyProdukt = item;
+        }
+    });
+    if (szukanyProdukt != null) {
+        biedronka.addProductToStore(szukanyProdukt);
+    }
+    else {
+        alert('Nie ma takiego produktu');
+    }
 }
 /**
  * 1.utworzenie pustej listy w celu przechowania elementów pozostałych po usunięciu jednego
@@ -39,11 +79,12 @@ function addProductToShop(item) {
  */
 function deleteProductFromShop(name) {
     let produktyDoZostawienia = [];
-    biedronka.listOfProducts.forEach((rzecz) => {
+    let produktyBiedronki = biedronka.getProducts();
+    produktyBiedronki.forEach((rzecz) => {
         if (rzecz.name !== name) {
             produktyDoZostawienia.push(rzecz);
         }
     });
-    biedronka.listOfProducts = produktyDoZostawienia;
+    biedronka.setProducts(produktyDoZostawienia);
     console.log(biedronka);
 }
--- app.ts
+++ app.ts
@@ -1,65 +1,116 @@
 type Unit = "g" | "ml" | "l"
 
 class Product {
-    
-    constructor(name:string, weight: number, value: Unit, packed: boolean ){
-     this.name = name;
-     this.weight = weight;
-     this.packed = packed;
-     this.value = value
+
+    constructor(name: string, weight: number, value: Unit, packed: boolean) {
+        this.name = name;
+        this.weight = weight;
+        this.packed = packed;
+        this.value = value
     }
     name: string;
     weight: number;
     packed: boolean;
     value: Unit
-    
+
 }
-let bread:Product = new Product ("chleb", 200, "g", true);
+let listaMagazynowa: Product[] = []
+
+let bread: Product = new Product("chleb", 200, "g", true);
 let cola: Product = new Product("cola", 2, "l", true);
-let butter:Product = new Product("masło", 250, "g", true);
-let milk:Product = new Product("mleko", 1, "l", false);
-let chocolade:Product = new Product("czekolada", 100,"g", true);
-let water:Product = new Product("woda", 1.5, "l", true)
+let butter: Product = new Product("masło", 250, "g", true);
+let milk: Product = new Product("mleko", 1, "l", false);
+let chocolade: Product = new Product("czekolada", 100, "g", true);
+let water: Product = new Product("woda", 1.5, "l", true)
+
+listaMagazynowa.push(bread);
+listaMagazynowa.push(cola);
+listaMagazynowa.push(butter);
+listaMagazynowa.push(milk);
+listaMagazynowa.push(chocolade);
+listaMagazynowa.push(water);
+
 
 class Shop {
-    constructor(name: string, city:string, listOfProducts:Product[] ){
+    constructor(name: string, city: string, listOfProducts: Product[]) {
         this.name = name;
         this.city = city;
         this.listOfProducts = listOfProducts;
 
     }
-    name:string;
-    city:string;
-    listOfProducts: Product[] = []
+    private name: string;
+    private city: string;
+    private listOfProducts: Product[] = []
+
+    getProducts(): Product[] {
+        // sprawdzic czy ten uzytkownik ma uprawnienia do pobrania produktow
+        return this.listOfProducts
+    }
+
+    existProduct(productName: string): boolean {
+        
+    }
+
+    setProducts(newListOfProducts: Product[]): void {
+        this.listOfProducts = newListOfProducts;
+    }
+
+    /**
+     * 
+     * 
+     */
+    addProductToStore(product: Product): void {
+     this.listOfProducts.push(product)
+    }
 
 }
 let biedronkaListOfProducts: Product[] = [bread, cola, butter];
-let miliczListOfProduct: Product[]=[milk, chocolade, water]
+let miliczListOfProduct: Product[] = [milk, chocolade, water]
 let biedronka: Shop = new Shop("Biedronka", "Szamotuły", biedronkaListOfProducts);
 let auchan: Shop = new Shop("Auchan", "Milicz", miliczListOfProduct);
+/**
+ * 1.utworzyc liste magazynowa
+ * 2. wyszukać na liscie magazynowej produkt mający name === parametr funcji 
+ * 3. sprawdzić czy szukany produkt istnieje
+ * a) istnieje, to dodać produkt do biedronki produkt za pomoca funkcji add product to store
+ * b) nie istnieje - alert "nie ma takiego produktu"
+ * 
+ */
+function addProductToShop(product: string) {
+    let szukanyProdukt: Product;
 
-function addProductToShop(item: Product){
-// let newProduct: Product = new Product("piwo", 500, "ml", true)
- 
-biedronka.listOfProducts.push(item)
-console.log(biedronka)
+
+    listaMagazynowa.forEach((item: Product) => {
+        if (item.name === product) {
+            szukanyProdukt = item
+        }
+    })
+
+
+    if (szukanyProdukt != null) {
+        biedronka.addProductToStore(szukanyProdukt)
+    } else {
+        alert('Nie ma takiego produktu')
+    }
 
 }
+
 /**
  * 1.utworzenie pustej listy w celu przechowania elementów pozostałych po usunięciu jednego
  * 2.literowanie po biedronka.lista produktów w celu zadecydowania czy produkt ma być usunięty czy zostawiony
  * a) jesli element ma tytuł === tytułowi z parametru funcji to nic
  * b) jesli element ma tytuł rózny od tutuły z parametru to dodaj go do pusta tymczasowa lista
  * 3) przypidanie poczatkowo pustej listy do biedronka.lista produktów
- */ 
-function deleteProductFromShop(name:string){
-    
+ */
+function deleteProductFromShop(name: string) {
+
     let produktyDoZostawienia: Product[] = []
-    biedronka.listOfProducts.forEach((rzecz)=>{
+    let produktyBiedronki = biedronka.getProducts();
+    produktyBiedronki.forEach((rzecz) => {
         if (rzecz.name !== name) {
-          produktyDoZostawienia.push(rzecz)
+            produktyDoZostawienia.push(rzecz)
         }
-        })
-        biedronka.listOfProducts = produktyDoZostawienia
-        console.log(biedronka)
+    })
+    biedronka.setProducts(produktyDoZostawienia)
+    console.log(biedronka)
 }
