f808d27803e9477427390eb451497ea8f8c4992c
Milena Sowinska <mil.sowinska@gmail.com>
2022-12-06 22:07:42 +0100

    created new functions ver 3

--- app.js
+++ app.js
@@ -1,14 +1,72 @@
 "use strict";
-/**Robi: dodawanie nowego produktu do listy produktów w danym sklepie
- * 1.utworzyc liste magazynowa
+class Product {
+    constructor(name, weight, value) {
+        this.name = name;
+        this.weight = weight;
+        this.value = value;
+    }
+}
+let listaMagazynowa = [];
+let bread = new Product("chleb", 100, "g");
+let water = new Product("woda", 1, "l");
+let cola = new Product('cola', 2, 'l');
+let cookies = new Product('ciastka', 100, 'g');
+let coffee = new Product('kawa', 200, "g");
+let juice = new Product(" sok", 3, "l");
+listaMagazynowa.push(bread);
+listaMagazynowa.push(water);
+listaMagazynowa.push(cola);
+listaMagazynowa.push(cookies);
+listaMagazynowa.push(coffee);
+listaMagazynowa.push(juice);
+class Shop {
+    constructor(name, city, type, productsList) {
+        this.productsList = [];
+        this.name = name;
+        this.city = city;
+        this.type = type;
+        this.productsList = productsList;
+    }
+    getProduct() {
+        return this.productsList;
+    }
+    addProductToStore(product) {
+        this.productsList.push(product);
+    }
+    setWarehouseState(aaa) {
+        this.productsList = aaa;
+    }
+}
+let biedronkaListaProduktów = [];
+biedronkaListaProduktów.push(bread);
+biedronkaListaProduktów.push(coffee);
+let biedronka = new Shop('biedronka', 'Poznan', 'dyskont', biedronkaListaProduktów);
+let auchanListaProduktów = [];
+auchanListaProduktów.push(cola);
+auchanListaProduktów.push(water);
+let auchan = new Shop('auchan', 'Warszawa', 'male', auchanListaProduktów);
+/**Robi: dodawanie nowego produktu do listy produktów w danym sklepie biedronka
+ * 1.wziąc liste magazynowa
  * 2. wyszukać na liscie magazynowej produkt mający name === parametr funcji
  * 3. sprawdzić czy szukany produkt istnieje
- * a) istnieje, to dodać produkt do biedronki produkt za pomoca funkcji add product to store
+ * a) istnieje, to dodać produkt do biedronki  za pomoca funkcji add product to store
  * b) nie istnieje - alert "nie ma takiego produktu"
  *
  */
-function addProductToShop(product) {
-    console.log(product);
+function addProductToShop(productName) {
+    let szukanyProdukt;
+    listaMagazynowa.forEach((cos) => {
+        if (cos.name === productName) {
+            szukanyProdukt = cos;
+        }
+    });
+    if (szukanyProdukt !== null) {
+        biedronka.addProductToStore(szukanyProdukt);
+    }
+    else {
+        alert("nie znaleziono");
+    }
+    console.log(biedronka);
 }
 /** Robi: usunięcie produktu z listy produktów w danym sklepie
  * 1.utworzenie pustej listy w celu przechowania elementów pozostałych po usunięciu jednego
@@ -17,15 +75,38 @@ function addProductToShop(product) {
  * b) jesli element ma tytuł rózny od tutuły z parametru to dodaj go do pusta tymczasowa lista
  * 3) przypidanie poczatkowo pustej listy do biedronka.lista produktów
  */
-function deleteProductFromShop(name) {
-    console.log(name);
+function deleteProductFromShop(productName) {
+    let produktyPoUsunieciu = [];
+    biedronka.getProduct().forEach((product) => {
+        if (product.name === productName) {
+        }
+        else {
+            produktyPoUsunieciu.push(product);
+        }
+    });
+    biedronka.setWarehouseState(produktyPoUsunieciu);
+    console.log(biedronka);
 }
 /**Robi: sprawdzenie czy w danym sklepie istnieje produkt o określonych parametrach
  *
  * funkcja ta okresla czy dany produkt istnieje w biedronce
+ * 1. utworzyć nową zmienną typu bulean
+ * 2. iterować po liście produktów sklepu biedronka
+ * 3. jeśli w biedronce istnieje produkt o nazwie === product name to
+ * - sprawdzone = true
+ * 4. w console log wyswietlic sprawdzone
  */
-function checkProduct(name) {
-    console.log(name);
+function checkProduct(productName) {
+    let sprawdzone = false;
+    biedronka.getProduct().forEach((product) => {
+        if (product.name === productName) {
+            sprawdzone = true;
+        }
+    });
+    if (sprawdzone === false) {
+        alert('nie ma takiego poduktu');
+    }
+    console.log(sprawdzone);
 }
 /** Robi: sprawdzenie czy w danym sklepie występuje produkt o danych unitach
  * 1.
@@ -34,25 +115,47 @@ function checkProduct(name) {
  * 3. wyswietl zawartosc w console log
  */
 function iloscProduktowLitry() {
-    console.log("aaaa");
+    let produktyOkresloneValue = [];
+    listaMagazynowa.forEach((produktZValue) => {
+        if (produktZValue.value === 'l') {
+            produktyOkresloneValue.push(produktZValue);
+        }
+    });
+    console.log(produktyOkresloneValue);
 }
-/**Robi: wyświetlenie informacji o danym produkcie
+/**
+ * funkcja ktora zwroci produkty z wartością wieksza lub równa dwa litry
+ */
+function iloscProduktowWiekszychLubRownych() {
+    let wiekszeLubRowne = [];
+    listaMagazynowa.forEach((productWeight) => {
+        if (productWeight.weight >= 2 && productWeight.value === 'l') {
+            wiekszeLubRowne.push(productWeight);
+        }
+    });
+    console.log(wiekszeLubRowne);
+}
+/**Robi: wyświetlenie informacji o produkcie z listy magazynowej na podstawie nazwy produktu
+ * 1. utworzyć pustą zmienną, w celu przypisania do niej wyszukanego produktu
+ * 2. iterować po liście magazynowej
+ * 3. jeśli nazwa produktu jest równa productName to przypisz to do nowo utworzonej zmiennej
+ * 4. wyswietl w console log zawartośc nowej zmiennej
  *
  */
-function showProductInformaction() {
-    console.log("product information");
+function showProductInformaction(productName) {
+    console.log(productName);
 }
 /** Robi: wyświetlenie zawartości sklepu biedronka
  *
  */
 function showBiedronka() {
-    console.log('pokazuję biedronkę');
+    console.log(biedronka);
 }
 /**
  *Robi:  wyświetlenie zawartości sklepu auchan
  */
 function showAuchan() {
-    console.log('pokazuję Auchan');
+    console.log(auchan);
 }
 /**Robi: Przeniesienie określonego produktu między sklepami
  *
